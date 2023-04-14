da640bde35bc65025e92c38bd38af6dea073ff0a
vamsi <sangeethavamsi1999@gmail.com>
2023-04-14 16:25:13 +0530

    initial checkin

---
+++ src/app/books/books.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-books',
+  templateUrl: './books.component.html',
+  styleUrls: ['./books.component.css']
+})
+export class BooksComponent {
+name1:string = 'clean Code Book';
+author1:string = 'Robert Good';
+sourceLink1:string ="https://m.media-amazon.com/images/I/81xIDx6-nHL._AC_UY218_.jpg"
+
+name2:string = 'Programatic Programmers';
+author2:string = 'David Good';
+sourceLink2:string ="https://m.media-amazon.com/images/I/71B4h-dSVzL._AC_UY218_.jpg"
+
+isDisabled:boolean = false;
+handleClick:boolean = false;
+}
